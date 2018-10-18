6799949ea477d121ebb3cb9b1410b6aa5246b1ca
David Calderwood <davidcalderwood@MacBook-Air-3.local>
2018-10-18 09:08:56 -0600

    React-Profile 18/10/18

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Profile from './component/Profile'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import About from './component/About'
+import Projects from './component/Projects'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router>
+      <div className="main-app">
+        <div className="main-frame">
+        <div className="components">
+          <Route exact path="/" component={Profile} />
+          <Route path="/about" component={About} />
+          <Route path="/Projects" component={Projects} />
+        </div>
+
+          <div className="right-menu">
+          <Link to="/">
+            <div className="navigate">
+            <p>Profile</p>
+            </div></Link>
+
+            <Link to="/Projects">
+            <div className="navigate">
+            <p>Projects</p>
+            </div></Link>
+
+            <Link to="/About">
+            <div className="navigate">
+              <p>About</p>
+            </div></Link>
+          </div>
+
+          <div className="footer">
+
+            <a className="footer-nav" target="_blank" rel="noopener noreferrer" href="https://github.com/davecalderwood">
+            <div >
+              GitHub
+            </div></a>
+
+            <a  className="footer-nav" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/david-calderwood-ba8b19172/">
+            <div>
+              LinkedIn
+            </div></a>
+
+            <a className="footer-nav" href="mailto:davidhcalderwood@gmail.com">
+            <div>
+              Email
+            </div></a>
+          </div>
+        </div>
       </div>
+      </Router>
     );
   }
 }
---
+++ src/component/Profile.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+          <center><br/>
+          <img className="Photo1" src="../Waterski.jpg" alt="Waterski"></img>
+
+          <h1 className="name">Dave Calderwood</h1>
+          </center>
+      </div>
+    );
+  }
+}
+
+export default Profile;
