4550d7f87634ccd03b0965e1d26c5f4c6882fe01
Dave Calderwood <davidhcalderwood@gmail.com>
2018-12-13 11:27:10 -0700

    updated about page

--- src/component/About.js
+++ src/component/About.js
@@ -6,7 +6,7 @@ class About extends Component {
       // The About page shows information about myself as well as Helio Training where I learned these skills, as well as a disclaimer for the background photo I used in the main body
       <div className="about-color">
           <h1>About</h1>
-          My name is David Calderwood and I am from Portland, Oregon. I am currently attending <a target="_blank" rel="noopener noreferrer" color="blue" href="https://heliotraining.com/">Helio Training</a>. Estimated Graduation is December, 19th 2018. Learning to be a junior developer.<br/>
+          My name is David Calderwood and I am from Portland, Oregon. I attendend <a target="_blank" rel="noopener noreferrer" color="blue" href="https://heliotraining.com/">Helio Training</a>. I graduated with proficiency as a Web Developer.<br/>
 
           <h2>Contact Information</h2>
           You can reach me at <b>(503)858-5790</b>(Portland area code but I live in Utah), or email me at <b>davidhcalderwood@gmail.com</b>. I also have provided a link to my email at the bottom. <br/><br/>
